035ac60a59798acc812b6dbb9d5e0c897a51feef
Brian Santos <brianmsantos81@gmail.com>
2018-04-05 12:06:32 -0700

    packages added

--- liri.js
+++ liri.js
@@ -1,2 +1,10 @@
 js
 require("dotenv").config();
+const keys = require('./keys.js')
+let [node, liri, action, input] = process.argv
+let twitter = require('twitter');
+let spotify = require('spotify');
+let request = require('request');
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
